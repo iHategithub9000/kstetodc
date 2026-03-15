d2c4b5d74272ade2168350d400a586597ee0d490
ketchupcomponent <101750090+iHategithub9000@users.noreply.github.com>
2026-03-15 22:50:37 +0100

    Draft fix announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -15,7 +15,7 @@ module.exports = {
   accessRestriction: RestrictionsEnum.DISCORD_PERMISSION,
   accessRestrictionArgs: PermissionsBitField.Flags.ManageMessages,
   name: "announce",
-  help_string: "<announcement_title> <announcement_content> <ping_everyone: true or false> - Sends an announcement in the channel where the command was ran. Your command is deleted after. Requires Manage Messages."
+  help_string: "<announcement_title> <announcement_content> <ping_everyone: true or false> - Sends an announcement in the channel where the command was ran. Your command is deleted after. Requires Manage Messages.",
   run: async (msg, argv, cl) => {
     argv = parseCLI(msg.content).slice(1)
     const ch = msg.channel;
@@ -24,6 +24,7 @@ module.exports = {
       .setTitle("📬 Announcement - "+argv[0])
       .setDescription(argv[1])
       .setColor(0x00ff00)
+
     ch.send({embeds:[embed], content: argv[2]=="true" ? "|| @everyone ||" : "|| Ping disabled ||")
   }
 }

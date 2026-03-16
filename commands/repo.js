216ba001f4f36a027ed763974619f9ac606e5797
ketchupcomponent <101750090+iHategithub9000@users.noreply.github.com>
2026-03-16 16:29:04 +0100

    Add invite command to send GitHub repository link

---
+++ commands/repo.js
@@ -0,0 +1,13 @@
+const { RestrictionsEnum } = require("../commandAccessRestrictions.js");
+
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    accessRestriction: RestrictionsEnum.NONE,
+    accessRestrictionArgs: "",
+    name: "invite",
+    help_string: "- Sends bot GitHub repository link",
+    run: async (msg, argv, cl) => {
+        msg.reply(`https://github.com/iHategithub9000/kstetodc`)
+    }
+}

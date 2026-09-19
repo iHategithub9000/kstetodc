cf06457a0137833899a4eb8a4b346121e5ac0b0d
ketchupcomponent <101750090+iHategithub9000@users.noreply.github.com>
2026-09-19 20:02:59 +0200

    Refactor guild leave logic to use allowed guild IDs
    Updated the guild leave logic to check against allowed guild IDs from a JSON file instead of hardcoded values.

--- events/leave_unknown_guilds.js
+++ events/leave_unknown_guilds.js
@@ -3,7 +3,7 @@ module.exports = {
     runOnce: false,
     run: async (_, cl) => {
         cl.guilds.cache.forEach(async g => {
-            if (g.id == "1444461932739432563" || g.id == "1446614371714334863") return;
+            if (require("allowedguildids.json").includes(g.id)) return;
 
             const firstChatable = g.channels.cache
                 .filter(c =>
